e5d2d69ca85190ab9103158087dea3be10b4dc73
Maximiliano Ibañez <ezemaxi@0345@gmail.com>
2022-08-03 19:55:31 -0300

    reactjs

--- src/Components/Cart/index.jsx
+++ src/Components/Cart/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { addDoc, collection, getFirestore } from 'firebase/firestore';
 import {  Link  } from 'react-router-dom'
 import { useCartContext } from "../../context/CartContext";
 import ItemCart from "../ItemCart";
@@ -8,6 +9,23 @@ const Cart = () => {
     
     const { cart, totalPrice } = useCartContext();
 
+    const order = { 
+        buyer: {    
+            name : 'Maxi',
+            email: 'ezemaxi0345@gmail.com',
+            phone: '123456',
+            adress:'asd123'
+        },
+        items: cart.map(product => ({ id: product.id, title: product.title, price: product.price, quantity: product.quantity})), 
+        total: totalPrice(),
+    }
+
+    const handleClick = () => {
+        const db = getFirestore();
+        const ordercollection = collection(db, 'orders');
+        addDoc(ordercollection, order)
+        .then(({ id }) => console.log(id))
+    }
 
         if (cart.length === 0){ 
             return (    
@@ -15,7 +33,7 @@ const Cart = () => {
                     <p>No hay elementos en el carrito</p>
                     <Link to='/'>Comprar</Link>
                 </>
-            )
+            );
         }
         
         return (   
@@ -27,6 +45,7 @@ const Cart = () => {
                 <p> 
                     total: {totalPrice()} 
                 </p>
+                <button onClick={handleClick}>Generar orden</button>
             </>
         )
 
